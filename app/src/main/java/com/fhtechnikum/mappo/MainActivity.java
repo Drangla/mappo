e689df6818d09dbfa829c0500e1c6345f4068beb
Drangla <mail-klaus@gmx.at>
2021-09-11 16:02:01 +0200

    Dashboard with Recyclerview&Cardview
    /java/CardViewItem added padding

--- app/src/main/java/com/fhtechnikum/mappo/MainActivity.java
+++ app/src/main/java/com/fhtechnikum/mappo/MainActivity.java
@@ -26,7 +26,23 @@ public class MainActivity extends AppCompatActivity {
         cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text4","Text5" ));
         cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text6","Text7" ));
         cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text8","Text9" ));
-
+        cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text6","Text7" ));
+        cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text8","Text9" ));
+        cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text6","Text7" ));
+        cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text8","Text9" ));
+        cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text6","Text7" ));
+        cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text8","Text9" ));
+        cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp, "Text1", "Text2"));
+        cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text2","Text3" ));
+        cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text4","Text5" ));
+        cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text6","Text7" ));
+        cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text8","Text9" ));
+        cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text6","Text7" ));
+        cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text8","Text9" ));
+        cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text6","Text7" ));
+        cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text8","Text9" ));
+        cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text6","Text7" ));
+        cardViewList.add(new CardViewItem(R.drawable.ic_android_black_24dp,"Text8","Text9" ));
 
         dashboardRecyclerView = findViewById(R.id.dashboard);
         //dashboardRecyclerView.setHasFixedSize(true); // when Recyclerview wont change in size, no matter how many items
